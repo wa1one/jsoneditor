fe0ceca398753423d402052aa866e9647a0634f6
Volodymyr Kovalchuk <vkovalc1@ford.com>
2022-08-18 19:04:09 +0300

    insertBefore

--- src/js/ModeSwitcher.js
+++ src/js/ModeSwitcher.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-import { ContextMenu } from './ContextMenu'
-import { translate } from './i18n'
+import { ContextMenu } from "./ContextMenu";
+import { translate } from "./i18n";
 
 /**
  * Create a select box to be used in the editor menu's, which allows to switch mode
@@ -12,112 +12,114 @@ import { translate } from './i18n'
  * @constructor
  */
 export class ModeSwitcher {
-  constructor (container, modes, current, onSwitch) {
+  constructor(container, modes, current, onSwitch) {
     // available modes
     const availableModes = {
       code: {
-        text: translate('modeCodeText'),
-        title: translate('modeCodeTitle'),
+        text: translate("modeCodeText"),
+        title: translate("modeCodeTitle"),
         click: function () {
-          onSwitch('code')
-        }
+          onSwitch("code");
+        },
       },
       form: {
-        text: translate('modeFormText'),
-        title: translate('modeFormTitle'),
+        text: translate("modeFormText"),
+        title: translate("modeFormTitle"),
         click: function () {
-          onSwitch('form')
-        }
+          onSwitch("form");
+        },
       },
       text: {
-        text: translate('modeTextText'),
-        title: translate('modeTextTitle'),
+        text: translate("modeTextText"),
+        title: translate("modeTextTitle"),
         click: function () {
-          onSwitch('text')
-        }
+          onSwitch("text");
+        },
       },
       tree: {
-        text: translate('modeTreeText'),
-        title: translate('modeTreeTitle'),
+        text: translate("modeTreeText"),
+        title: translate("modeTreeTitle"),
         click: function () {
-          onSwitch('tree')
-        }
+          onSwitch("tree");
+        },
       },
       view: {
-        text: translate('modeViewText'),
-        title: translate('modeViewTitle'),
+        text: translate("modeViewText"),
+        title: translate("modeViewTitle"),
         click: function () {
-          onSwitch('view')
-        }
+          onSwitch("view");
+        },
       },
       preview: {
-        text: translate('modePreviewText'),
-        title: translate('modePreviewTitle'),
+        text: translate("modePreviewText"),
+        title: translate("modePreviewTitle"),
         click: function () {
-          onSwitch('preview')
-        }
-      }
-    }
+          onSwitch("preview");
+        },
+      },
+    };
 
     // list the selected modes
-    const items = []
+    const items = [];
     for (let i = 0; i < modes.length; i++) {
-      const mode = modes[i]
-      const item = availableModes[mode]
+      const mode = modes[i];
+      const item = availableModes[mode];
       if (!item) {
-        throw new Error('Unknown mode "' + mode + '"')
+        throw new Error('Unknown mode "' + mode + '"');
       }
 
-      item.className = 'jsoneditor-type-modes' + ((current === mode) ? ' jsoneditor-selected' : '')
-      items.push(item)
+      item.className =
+        "jsoneditor-type-modes" +
+        (current === mode ? " jsoneditor-selected" : "");
+      items.push(item);
     }
 
     // retrieve the title of current mode
-    const currentMode = availableModes[current]
+    const currentMode = availableModes[current];
     if (!currentMode) {
-      throw new Error('Unknown mode "' + current + '"')
+      throw new Error('Unknown mode "' + current + '"');
     }
-    const currentTitle = currentMode.text
+    const currentTitle = currentMode.text;
 
     // create the html element
-    const box = document.createElement('button')
-    box.type = 'button'
-    box.className = 'jsoneditor-modes jsoneditor-separator'
-    box.textContent = currentTitle + ' \u25BE'
-    box.title = translate('modeEditorTitle')
+    const box = document.createElement("button");
+    box.type = "button";
+    box.className = "jsoneditor-modes jsoneditor-separator";
+    box.textContent = currentTitle + " \u25BE";
+    box.title = translate("modeEditorTitle");
     box.onclick = () => {
-      const menu = new ContextMenu(items)
-      menu.show(box, container)
-    }
+      const menu = new ContextMenu(items);
+      menu.show(box, container);
+    };
 
-    const frame = document.createElement('div')
-    frame.className = 'jsoneditor-modes'
-    frame.style.position = 'relative'
-    frame.appendChild(box)
+    const frame = document.createElement("div");
+    frame.className = "jsoneditor-modes";
+    frame.style.position = "relative";
+    frame.appendChild(box);
 
-    container.appendChild(frame)
+    container.insertBefore(frame, container.firstChild);
 
     this.dom = {
       container,
       box,
-      frame
-    }
+      frame,
+    };
   }
 
   /**
    * Set focus to switcher
    */
-  focus () {
-    this.dom.box.focus()
+  focus() {
+    this.dom.box.focus();
   }
 
   /**
    * Destroy the ModeSwitcher, remove from DOM
    */
-  destroy () {
+  destroy() {
     if (this.dom && this.dom.frame && this.dom.frame.parentNode) {
-      this.dom.frame.parentNode.removeChild(this.dom.frame)
+      this.dom.frame.parentNode.removeChild(this.dom.frame);
     }
-    this.dom = null
+    this.dom = null;
   }
 }
